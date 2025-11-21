ad3bf0a7d47ecf520272276dc7867f45eb9f83b8
wsxiaoyaoyou <836167179@qq.com>
2025-11-21 23:00:16 +0800

    yml test

--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ from app import app
 def test_ping():
     resp = app.test_client().get("/api/ping")
     assert resp.status_code == 200
-    assert resp.json["ping"] == "pong"
+    assert resp.json["ping"] == "hello"
